c95afdfdb3b54aa1b7ea1136f5051700e3d9864c
hieule247 <54555669+hieule247@users.noreply.github.com>
2020-11-17 10:02:24 -0600

    15-nov

---
+++ iTravel2020/src/main/java/itravel/controller/AuthenticationFilter.java
@@ -0,0 +1,36 @@
+package itravel.controller;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class AuthenticationFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException, IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+
+        String action = req.getServletPath();
+        if("/".equals(action) || "/login".equals(action) || "/login.jsp".equals(action)
+                || "/loginServlet".equals(action)
+                || "/index.jsp".equals(action)){
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else{
+            Object isLoggedObj = req.getSession().getAttribute("isLogged");
+            if(isLoggedObj != null){
+                boolean isLoggedIn = (Boolean) isLoggedObj;
+                if(isLoggedIn){
+                    filterChain.doFilter(servletRequest, servletResponse);
+                    return;
+                }
+            }
+//            http://localhost:8080/filter_demo_war_exploded/
+            String path = req.getContextPath()+ "/";
+            resp.sendRedirect(path);
+        }
+//        System.out.println("AuthenticationFilter ---2-------");
+    }
+}
